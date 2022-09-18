9966eb8737afc59fbbd6e05a7b249143732f0e2c
Dimitri Devoille <dimitri.devoille@digitregroup.com>
2022-09-18 17:05:09 +0200

    Add login API

--- pages/login.js
+++ pages/login.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { signIn, useSession } from 'next-auth/react'
 import Link from 'next/link'
 import Layout from '../components/Layout'
 import { useForm } from 'react-hook-form'
-
+import { getError } from '../utils/error'
+import { toast } from 'react-toastify'
+import { useRouter } from 'next/router'
 
 
 export default function LoginScreen() {
+
+  const { data: session } = useSession()
+
+  const router = useRouter()
+  const { redirect } = router.query
+
+  useEffect(() => {
+    if (session?.user) {
+      router.push(redirect || '/')
+    }
+  }, [session, router, redirect])
 
   const {
     handleSubmit,
@@ -13,8 +27,19 @@ export default function LoginScreen() {
     formState: { errors },
   } = useForm()
 
-  const submitHandler = ({ email, password }) => {
-    console.log(email, password);
+  const submitHandler = async ({ email, password }) => {
+    try {
+      const result = await signIn('credentials', {
+        redirect: false,
+        email,
+        password,
+      })
+      if (result.error) {
+        toast.error(result.error);
+      }
+    } catch (err) {
+      toast.error(getError(err));
+    }
   }
 
   return (
